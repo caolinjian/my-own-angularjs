4cd5048cb6553f6f693e652d8deab92f8d558734
phoenix <phoenix@zenzet.com>
2016-06-21 16:34:26 +0800

    init test

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,12 +1,30 @@
-var fs = require('fs');
-
-
 module.exports = function(grunt) {
     require('load-grunt-tasks')(grunt);
     grunt.initConfig({
         eslint: {
             all: ['src/**/*.js']
+        },
+        testem: {
+            unit: {
+                options: {
+                    framework: 'jasmine2',
+                    launch_in_dev: ['PhantomJS'],
+                    before_tests: 'grunt eslint',
+                    serve_files: [
+                        'node_modules/lodash/lodash.js',
+                        'node_modules/jquery/dist/jquery.js',
+                        'node_modules/sinon/pkg/sinon.js',
+                        'src/**/*.js',
+                        'test/**/*.js'
+                    ],
+                    watch_files: [
+                        'src/**/*.js',
+                        'test/**/*.js'
+                    ]
+                }
+            }
         }
     });
-    grunt.registerTask('default', ['eslint']);
+    grunt.loadNpmTasks('grunt-contrib-testem');
+    grunt.registerTask('default', ['testem:run:unit']);
 };
